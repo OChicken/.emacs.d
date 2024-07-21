5ff66c4a63d75080bd045a663d7940c8140354ea
OChicken <shouran.ma@gmail.com>
2024-07-21 16:20:44 +0200

    math-matrixp OK

--- lisp/init-math.el
+++ lisp/init-math.el
@@ -7,15 +7,29 @@
 
 ;;; Code:
 
-(require 'calc)
+;; (require 'calc)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                                         ;                Matrix               ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun math-matrixp (a)
+  "True if A is a matrix.  [P x] [Public]."
+  (and (vectorp a)
+       (vectorp (aref a 0))
+       (> (length a) 0)
+       (> (length (aref a 0)) 0)
+       (let ((len (length (aref a 0))))
+         (catch 'not-a-matrix
+           (dotimes (i (length a))
+             (if (or (not (vectorp (aref a i)))
+                     (/= (length (aref a i)) len))
+                 (throw 'not-a-matrix nil)))
+           t))))
+
 (defun math-mat-idx (A m n)
   "Get the index M,N from matrix A."
-  (nth n (nth m A)))
+  (aref (aref A m) n))
 
 (provide 'init-math)
 ;;; init-math.el ends here
