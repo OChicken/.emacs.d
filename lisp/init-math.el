2a2a16c12657d2dbea353749c10b763980636f62
OChicken <shouran.ma@gmail.com>
2024-07-22 07:48:10 +0200

    math-identity-matrix-p OK

--- lisp/init-math.el
+++ lisp/init-math.el
@@ -40,10 +40,39 @@
     (and (cdr dim)
 	 (= (car dim) (nth 1 dim)))))
 
+(defun math-identity-matrix-p (mat &optional mul)
+  "True if MAT is an identity matrix.  Accept MUL as t to take (0,0) of MAT."
+  (if (math-square-matrixp mat)
+      (let ((a (if mul
+                   (nth 0 (nth 0 mat))
+                 1))
+            (n (length mat))
+            (i 0))
+        (while (and (< i n)
+                    (math-ident-row-p (nth i mat) i a))
+          (setq i (1+ i)))
+        (if (= i n)
+            t
+          nil))))
+
+(defun math-ident-row-p (row n &optional a)
+  "Given ROW and N th elm A, predicate its left & right elms are all 0."
+  (unless a
+    (setq a 1))
+  (and
+   (not (memq nil (mapcar
+                   (lambda (x) (eq x 0))
+                   (nthcdr (1+ n) row))))
+   (not (memq nil (mapcar
+                   (lambda (x) (eq x 0))
+                   (butlast
+                    (cdr row)
+                    (- (length row) n)))))
+   (eq (elt row n) a)))
 
 (defun math-mat-idx (A m n)
   "Get the index M,N from matrix A."
-  (aref (aref A m) n))
+  (nth n (nth m A)))
 
 (provide 'init-math)
 ;;; init-math.el ends here
