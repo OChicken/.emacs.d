10831fc00e9b083efa7e9874348efd012e6982d3
OChicken <shouran.ma@gmail.com>
2024-10-16 00:11:37 +0200

    version number 26.1

--- init.el
+++ init.el
@@ -9,9 +9,6 @@
 ;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
 ;;(setq debug-on-error t)
 
-(let ((minver "26.1"))
-  (when (version< emacs-version minver)
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
 (when (version< emacs-version "27.1")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
--- lisp/init-lite.el
+++ lisp/init-lite.el
@@ -1,6 +1,21 @@
-;;; init-lite.el --- Basic settings tailored to legacy or pure tty machines -*- lexical-binding: t -*-
+;;; init-lite.el --- Lite settings -*- lexical-binding: t -*-
+;;;
 ;;; Commentary:
+;;;
+;;; I call it `lite' because I just want to keep the basic functionalities on
+;;; the legacy OS or pure tty machines.  No packages will be installed on this
+;;; config.
+;;;
+;;;
 ;;; Code:
+
+(let ((minver "26.1"))
+  ; "26.1" is the version number appears in Purcell's config on 20230905.
+  ; If this file is suitable for an older Emacs version, I shall decrease this
+  ; number.
+  (when (version< emacs-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+
 
 
 (provide 'init-lite)
