2692f0dd20e0eeb41658dfc36bf41a37dff287ad
OChicken <shouran.ma@gmail.com>
2024-10-15 19:02:05 +0200

    init init-lite.el

---
+++ lisp/init-lite.el
@@ -0,0 +1,7 @@
+;;; init-lite.el --- Basic settings tailored to legacy or pure tty machines -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+(provide 'init-lite)
+;;; init-lite.el ends here
