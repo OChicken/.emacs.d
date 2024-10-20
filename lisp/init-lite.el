801be22ca8790532a9ede1a0f97b1ff5e93845f4
OChicken <shouran.ma@gmail.com>
2024-10-20 02:29:40 +0200

    mark vscode-dark NOT avail before 25.2.2 since not work on 24.5.1

--- lisp/init-lite.el
+++ lisp/init-lite.el
@@ -94,8 +94,12 @@ be disabled. Please upgrade if possible." emacs-version))
 ; An accurate port of the default Visual Studio Code Dark+ theme for Emacs
 ; https://github.com/ianyepan/vscode-dark-plus-emacs-theme
 ; Modified by me
-(customize-set-variable 'vscode-dark-plus-box-org-todo nil)
-(load-theme 'vscode-dark t)
+; Features in this theme is NOT available at Emacs 24.5.1 on Ubuntu 16.04 LTS
+; So I mark it available for the Emacs 25.2.2 on Ubuntu 18.04.5 LTS
+
+(unless (version< emacs-version 25.2.2)
+  (customize-set-variable 'vscode-dark-plus-box-org-todo nil)
+  (load-theme 'vscode-dark t))
 
 
 ;; delsel.el --- delete selection if you insert ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
