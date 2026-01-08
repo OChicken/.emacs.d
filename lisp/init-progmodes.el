fbec925f7700a66b48a41ce9082fdccc87f11e31
OChicken <shouran.ma@gmail.com>
2026-01-08 04:50:19 +0100

    rm lisp & slime settings: I used to be a fan of Lisp, but not anymore

--- lisp/init-progmodes.el
+++ lisp/init-progmodes.el
@@ -237,56 +237,6 @@ Feel free to use command to toggle between them."
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                    Lisp                                    ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; (add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
-(add-to-list 'load-path (concat (expand-file-name user-emacs-directory)
-                                "elpa/slime-20251222.1632/contrib"))
-
-(define-key lisp-mode-map (kbd "C-M-q") 'scroll-down-line) ; indent-sexp
-(add-hook 'lisp-mode-hook
-          (lambda ()
-            (unless (featurep 'slime)
-              (require 'slime)
-              (normal-mode))))
-
-(require 'slime-repl)
-
-(with-eval-after-load 'slime
-  (define-key slime-repl-mode-map (kbd "M-q") 'slime-repl-previous-input)
-  (define-key slime-repl-mode-map (kbd "M-z") 'slime-repl-next-input))
-
-;; From http://bc.tech.coop/blog/070515.html
-(defun lispdoc ()
-  "Search lispdoc.com for SYMBOL: the symbol currently under the cursor."
-  (interactive)
-  (let* ((word-at-point (word-at-point))
-         (symbol-at-point (symbol-at-point))
-         (default (symbol-name symbol-at-point))
-         (inp (read-from-minibuffer
-               (if (or word-at-point symbol-at-point)
-                   (concat "Symbol (default " default "): ")
-                 "Symbol (no default): "))))
-    (if (and (string= inp "") (not word-at-point) (not
-                                                   symbol-at-point))
-        (message "you didn't enter a symbol!")
-      (let ((search-type (read-from-minibuffer
-                          "full-text (f) or basic (b) search (default b)? ")))
-        (browse-url (concat "http://lispdoc.com?q="
-                            (if (string= inp "")
-                                default
-                              inp)
-                            "&search="
-                            (if (string-equal search-type "f")
-                                "full+text+search"
-                              "basic+search")))))))
-(define-key lisp-mode-map (kbd "C-c l") 'lispdoc)
-
-(slime-setup)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                   Haskell                                  ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
