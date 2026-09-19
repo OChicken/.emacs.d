e113d78fa4234f3d99006d0423aa8bf3d43d9508
OChicken <shouran.ma@gmail.com>
2026-09-19 14:28:27 +0200

    rm eat-mode-hook

--- lisp/init-progmodes.el
+++ lisp/init-progmodes.el
@@ -51,7 +51,7 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                                        ;              Projectile             ;
+;;                                 Projectile                                 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Project Interaction Library for Emacs
@@ -187,9 +187,6 @@ Feel free to use command to toggle between them."
 ; Hint of shortcuts to toggle:
 ; C-c C-e: eat-emacs-mode (select Claude's answer)
 ; C-c C-j: eat-semi-char-mode (continue to ask Claude)
-(add-hook 'eat-mode-hook
-          (lambda () (setq-local window-adjust-process-window-size-function
-                                 #'window-adjust-process-window-size-largest)))
 (add-to-list 'claudemacs-process-environment "CLAUDE_CODE_NO_FLICKER=1")
 
 
